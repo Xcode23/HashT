1e9522fc9f01572f39b1c1930bc1a309560da9a0
Xcode23 <joaofarinha10@hotmail.com>
2016-05-07 13:41:17 +0100

    resize e aux

--- HashT.c
+++ HashT.c
@@ -14,38 +14,119 @@ typedef struct _node_{
 struct _hashtable_{
   int (*hashcode)(void*);
   int (*equals)(void*,void*);
+  void* (*clonekey)(void*);
+  void* (*clonevalue)(void*);
   int size;
   int used;
   node** table;
 };
 
-htable* newTable(int (*hashfunc)(void*), int (*equalsfunc)(void*,void*)){
+htable* newTable(int (*hashfunc)(void*), int (*equalsfunc)(void*,void*), void* (*clonekeyfunc)(void*), void* (*clonevaluefunc)(void*)){
   htable* newtable;
+
   if(!(newtable=(htable*)malloc(sizeof(htable))))
-    return MEM_ERROR;
+    return NULL;
+
   newtable->hashcode=hashfunc;
   newtable->equals=equalsfunc;
+  newtable->clonekey=clonekeyfunc;
+  newtable->clonevalue=clonevaluefunc;
   newtable->size=STARTINGSIZE;
   newtable->used=0;
   if(!(newtable->table=(node**)malloc(newtable->size*sizeof(node*))))
-    return MEM_ERROR;
+    return NULL;
+
   return newtable;
 }
 
-int insert(htable* hashtable, void* key, void* value){
-  int location=hashtable->hashcode(key);
-  node* auxnode;
-  if(location<0)return KEY_TYPE_ERROR;
+void erasenode(node* oldnode){
+  free(oldnode->key);
+  free(oldnode->value);
+  free(oldnode);
+}
+
+void eraselist(node* oldlist){
+  node* next;
+  while(oldlist!=NULL){
+    next=oldlist->next;
+    erasenode(oldlist);
+    oldlist=next;
+  }
+}
+
+void* getkey(node* node){
+  return node->key;
+}
+
+void* getvalue(node* node){
+  return node->value;
+}
+
+htable* resize(htable* hashtable){
+  node **newtable, **oldtable, *auxnode;
+  oldtable=hashtable->table;
+  int i,oldsize=hashtable->size;
+
+  hashtable->size=hashtable->size*2;
+  if(!(newtable=(node**)malloc(hashtable->size*sizeof(node*))))
+    return NULL;
+
+  hashtable->table=newtable;
+  hashtable->used=0;
+
+
+  for(i=0;i<oldsize;i++){
+    if(oldtable[i]){
+      auxnode=oldtable[i];
+      while(auxnode!=NULL){
+        insert(hashtable,getkey(auxnode),getvalue(auxnode));
+        auxnode=auxnode->next;
+      }
+      eraselist(oldtable[i]);
+    }
+  }
+  free(oldtable);
+
+  return hashtable;
+}
+
+
+
+void* insert(htable* hashtable, void* key, void* value){
+  int location,equal=0;
+  node *auxnode, *newnode;
+  key=hashtable->clonekey(key);
+  value=hashtable->clonevalue(value);
+
+  location=hashtable->hashcode(key);
+
+  if(location<0)return NULL;
+
+  if((float)hashtable->used/(float)hashtable->size>LOADFACTOR)
+    if(!resize(hashtable))return NULL;
+
   location=location%hashtable->size;
-  node* newnode=(node*)malloc(sizeof(node));
+
+  if(!(newnode=(node*)malloc(sizeof(node))))
+    return NULL;
+
   newnode->key=key;
   newnode->value=value;
+
   if(hashtable->table[location]==NULL)
     hashtable->table[location]=newnode;
   else{
     auxnode=hashtable->table[location];
-    while(auxnode->next)auxnode=auxnode->next;
+    while(auxnode->next&&(!(equal=hashtable->equals(auxnode->next->key,key))))
+      auxnode=auxnode->next;
+    if(equal){
+      newnode->next=auxnode->next->next;
+      erasenode(auxnode->next);
+      hashtable->used--;
+    }
     auxnode->next=newnode;
   }
-  return OK;
+  hashtable->used++;
+
+  return value;
 }
--- HashT.h
+++ HashT.h
@@ -1,12 +1,9 @@
 #ifndef HASHT
 #define HASHT
 
-#define MEM_ERROR NULL
-#define KEY_TYPE_ERROR -1
-#define OK 0
-
 typedef struct _hashtable_ htable;
 
-htable* newTable(int (*hashfunc)(void*), int (*equalsfunc)(void*,void*));
+htable* newTable(int (*hashfunc)(void*), int (*equalsfunc)(void*,void*), void* (*clonekeyfunc)(void*), void* (*clonevaluefunc)(void*));
+void* insert(htable* hashtable, void* key, void* value);
 
 #endif
